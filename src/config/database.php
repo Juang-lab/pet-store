82d898a7afd16553038321da495389f63df557ea
Juan <jugarc9427@gmail.com>
2025-05-07 21:39:18 -0500

    ok

--- src/config/database.php
+++ src/config/database.php
@@ -1,34 +1,32 @@
-<?php 
-    $host     = "aws-0-us-east-1.pooler.supabase.com";
-    $port     = "6543";
-    $dbname   = "postgres";
-    $user     = "postgres.beiffshkcrbekdbyfsiu";
-    $password = "unicesmag@@";
-
-   /* $host     = "localhost";
-    $port     = "5432";
-    $dbname   = "petstore";
-    $user     = "postgres";
-    $password = "postgres";*/
-
-
-    $data_connection ="
+<?php
+     $host        ="aws-0-us-east-2.pooler.supabase.com";
+     $port        ="6543";
+     $dbname      ="postgres";
+     $user        ="postgres.tprexcklrcpreuchonwl";
+     $password    ="unicesmag@@";
+    /*
+    $host        ="localhost";
+    $port        ="5432";
+    $dbname      ="petstore";
+    $user        ="postgres";
+    $password    ="postgres";
+*/
+    $data_connection="
         host=$host
-        port=$port  
+        port=$port
         dbname=$dbname
         user=$user
-        password=$password"
-        ;
-    $conn= pg_connect($data_connection);
+        password=$password
+    ";
+    
+    $conn = pg_connect($data_connection);
 
     if(!$conn){
-        echo "Connection error"; }
-        else {
-            echo" Success !!!";
+        echo "Connection error";
 
-       }
+    }else{
+        echo "Success !!!";
+    }
 
-       //pg_close($conn);
-
-    
+    //pg_close($conn);
 ?>
